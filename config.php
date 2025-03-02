7812736a92389ed11486b3d5a48e66602bdaa780
PikaaxYT <111752386+PikaaxYT@users.noreply.github.com>
2025-03-02 12:11:19 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'host' => 'Adresse IP de votre base SQL',
+    'user' => 'Nom d'utilisateur de votre base SQL',
+    'pwd' => 'Mot de passe de votre base SQL',
+    'db' => 'Nom de votre base de données',
+];
+?>
