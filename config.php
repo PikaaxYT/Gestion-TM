f1241d86c81fc6104b348da92df05d610e562de1
PikaaxYT <111752386+PikaaxYT@users.noreply.github.com>
2025-03-22 15:15:45 +0100

    Version 1.1

--- config.php
+++ config.php
@@ -1,1 +1,8 @@
-
+<?php
+return [
+    'host' => '[ADRESSE IP DU SERVEUR]',
+    'user' => '[NOM D'UTILISATEUR]',
+    'pwd' => '[MOT DE PASSE]',
+    'db' => '[NOM DE VOTRE BASE DE DONNÉES SQL]',
+];
+?>
